5de8a5677d892573bd7c546ceb48bb1881a0857e
josefinenilsson <josefinenilsson@wlanguest86178.eduroam.hj.se>
2013-12-02 09:56:37 +0100

    Added more structure comments and added the db_AddUser()
    We need to finish the db_AddUser()

--- DB.php
+++ DB.php
@@ -14,6 +14,12 @@ class Database extends mysqli {
         }
     }
 	
+#################################################################################################################################################
+											/* DATABASE PRODUCT FUNCTIONS START */
+#################################################################################################################################################
+												 
+												 
+	
 /* 	########################### 
 		Add product 
 	########################### */
@@ -34,20 +40,52 @@ class Database extends mysqli {
 	}
 	
 /* 	########################### 
-		Chanhe product
+		Change product
 	########################### */
 	
 	public function db_ChangeProduct(){
-		$this->query('UPDATE "Products" SET "Name"="korv" WHERE "Name"="Depend Blue"');
+		$this->query('UPDATE "Products" SET "$productName"="$newProductName" WHERE "Name"="Depend Blue"');
 	}
 	// Here we need to figure out what we want to change in the change function, and add variabels.
+	
+	
+	
+#################################################################################################################################################
+											/* DATABASE PRODUCT FUNCTIONS ENDS */
+#################################################################################################################################################
+												 
+												 
+	
+#################################################################################################################################################
+											/* DATABASE USER FUNCTIONS START */
+#################################################################################################################################################
+	
+	
+	
+/* 	########################### 
+		Add User
+	########################### */
+	
+	public function db_AddUser(){
+		$this->query('INSERT INTO `User`(`Firstname`, `Lastname`, `Email`, `Adress`, `SSN`, `Phonenumber`, `Password`, `isAdmin`) VALUES ("Josefine","Nilsson","josefine.h.nilsson93@gmail.com","Hermansvägen 104","199302184966","0766120150","hejhej","false")');
+		echo "db_AddUser()";
+	}
+	// We need to add variables here instead of my name stuff
+	
+	
+#################################################################################################################################################
+											/* DATABASE USER FUNCTIONS ENDS */
+#################################################################################################################################################
+	
+	
 	
 } // Here ends the class Database
 
 $db = new Database('localhost', 'root', 'root', 'webshop'); // Connects to database here
-//$db->db_AddProduct(); 									   	// Add product function runs here
+//$db->db_AddProduct(); 									// Add product function runs here
 //$db->db_DeleteProduct();									// Delete product function runs here
 //$db->db_ChangeProduct();									// Change product function runs here
+$db->db_AddUser();											// Add user function runs here
 
 
 echo 'Success... ' . $db->host_info . "\n";
