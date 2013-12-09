0091350dbf909df616be33b138be667856919aab
Amelia Catalán <amelia_catalan@hotmail.com>
2013-12-09 20:43:25 +0100

    börjat med css på adduser sidan
    bli inte avskräckt av hur det ser ut just nu!

--- adduser.php
+++ adduser.php
@@ -1,5 +1,26 @@
 <meta http-equiv="content-type"
       content="text/html;charset=utf-8" />
+ <head>
+        <link rel="stylesheet" type="text/css" href="base.css">
+    </head
+    <body>
+<div id="wrapper">
+<header id="header">
+    <h1 class="h1title">Webshop</h1>
+</header>
+
+<nav id="mainmenu">
+    <ul>
+        <li><a href="#">Startsida</a></li>
+        <li><a href="#">Produkter</a></li>
+        <li><a href="#">Frågor och Svar</a></li>
+        <li><a href="#">Om oss</a></li>
+    </ul>
+</nav>
+
+<article>
+    <img src="img/depend.jpg">
+    <section id="login">
 
 <form action="adduser.php" method="post" enctype="multipart/form-data">
         <table>
@@ -36,6 +57,10 @@
                 <td><input type="submit" name="submit"</td>
             </tr>
         </table>
+    </section>
+</article>
+<footer> Kontakta oss på webshop@skolarbete.se</footer>
+</body>        
 
 <?php
 
