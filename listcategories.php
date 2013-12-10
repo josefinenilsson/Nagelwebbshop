27d4eb2eba4de62b7a71d2c7ac95ebf0060f8e61
josefinenilsson <josefinenilsson@Togetic.local>
2013-12-10 19:38:12 +0100

    updates list categories

--- listcategories.php
+++ listcategories.php
@@ -9,10 +9,10 @@ var_dump($categories);
 if($categories == false){
      return "Error";
 }  else {
-    for ($i = 0; $i < $categories.count(); $i++){ 
+    for ($i = 0; $i < count($categories); $i++){
         echo '<div>';
-        echo '<a href="/?page=categories&category='.categories[$i]['Category_ID'].'"><img src="'.categories[$i]['Category_image'].'"></a>';
-		echo '<h3>'.categories[$i]['Name'].'</h3>';
+        echo '<a href="/?page=categories&category='.$categories[$i]['Category_ID'].'"><img src="'.$categories[$i]['Category_image'].'"></a>';
+		echo '<h3>'.$categories[$i]['Name'].'</h3>';
         echo '</div>';
     }
 }
