f67f04e31e9f2b4a538059408c2d873facda8ea6
Amelia Catalán <amelia_catalan@hotmail.com>
2014-01-22 11:02:11 +0100

    kundkorg och om oss typ

--- about.php
+++ about.php
@@ -1,7 +1,10 @@
 <?php
 include "header.php";
 ?>
-<h2>Här kör vi lite fin utfyllnadstext</h2>
+    <article>
+        <h2>Om oss</h2>
+        <p>Vi är två tjejer som går det andra året på programmet Grafisk design och Webbutveckling på Tekniska Högskolan i Jönköping. Denna webbshop började vi göra under hösterminen 2013 då vi gick kursen Webb och databaser som är 6 högskolepoäng. </p>
+    </article>
 <?php 
 include "footer.php";
 ?>
---
+++ order.php
@@ -0,0 +1,18 @@
+<?php
+ include "header.php";
+?>
+
+<article>
+    <h3>I min varukorg</h3>
+    <h3>Pris</h3>
+    <h3>Antal</h3>
+    <h3>Ta bort</h3>
+    
+    <button type="button">X</button>
+  
+    <div id="ordernow">
+    <button type="button">Slutför beställning</button>
+    <p>Genom att trycka på Slutför beställning bekräftar du att din beställning kommer att skickas  inom 3-5 dagar tillsammans med din faktura.</p>
+    </div>
+    
+</article>
