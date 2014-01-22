823e54ac93e19903d2e453dc744f6b000d2c249d
josefinenilsson <josefinenilsson@wlanguest136214.eduroam.hj.se>
2014-01-22 14:35:13 +0100

    adminproducts.php ser lite bättre ut nu

--- adminproducts.php
+++ adminproducts.php
@@ -14,11 +14,11 @@ if (isset($_POST['delete'])){ // Om man trycker på kanppen 'delete' kör funkti
 }
 $products[] = $db->db_ListProducts(); // Hämta en array genom funktionen db_ListProducts()
 
-echo '<table border="1">';
+		echo '<table class="admin_table" border="1">';
 		echo '<tr>';
-		echo '<th>Serienummer</th>';
-		echo '<th>Produktnamn</th>';
-		echo '<th>Ta bort produkt</th>';
+		echo '<th class="admin_th">Serienummer</th>';
+		echo '<th class="admin_th">Produktnamn</th>';
+		echo '<th class="admin_th">Ta bort produkt</th>';
 		echo '</tr>';
 		echo '</table>';
 
@@ -27,11 +27,11 @@ if($products == false){ // Om den inte kan hämta produktlistan genom db_ListPro
 }  else {
     for ($i = 0; $i < count($products[0]); $i++){ // Om den hittar produkterna kör en for loop. Som skriver ut en tabell med de olika värderna.
         	
-		echo '<table border="1">';
+		echo '<table class="admin_table" border="1">';
 		echo '<tr>';
-		echo '<td>'.$products[0][$i]["SerialNumber"].'</td>';
-		echo '<td>'.$products[0][$i]["Name"].'</td>';
-		echo '<td><form action="/adminproducts.php" method="post"><input type="submit" value="'.$products[0][$i]["SerialNumber"].'" name="delete"></form></td>';
+		echo '<td class="admin_td">'.$products[0][$i]["SerialNumber"].'</td>';
+		echo '<td class="admin_td">'.$products[0][$i]["Name"].'</td>';
+		echo '<td class="admin_td"><form action="/adminproducts.php" method="post"><input type="submit" value="'.$products[0][$i]["SerialNumber"].'" name="delete"></form></td>';
 		echo '</tr>';
 		echo '</table>';
 					
