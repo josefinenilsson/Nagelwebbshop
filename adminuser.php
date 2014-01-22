b215df4672fd9eaa2db5196b93d93ee8dfbf0da8
josefinenilsson <josefinenilsson@wlanguest136214.eduroam.hj.se>
2014-01-22 14:37:44 +0100

    adminuser.php ser lite finare ut

--- adminuser.php
+++ adminuser.php
@@ -14,10 +14,10 @@ if (isset($_POST['delete'])){ // Om man trycker på kanppen 'delete' kör funkti
 }
 $user[] = $db->db_ListUser(); // Hämta en array genom funktionen db_ListUser()
 
-echo '<table border="1">';
+echo '<table class="admin_table" border="1">';
 		echo '<tr>';
-		echo '<th>Personnummer</th>';
-		echo '<th>Ta bort användare</th>';
+		echo '<th class="admin_th">Personnummer</th>';
+		echo '<th class="admin_th">Ta bort användare</th>';
 		echo '</tr>';
 		echo '</table>';
 
@@ -26,10 +26,10 @@ if($user == false){ // Om den inte kan hämta användarlistan genom db_ListUser(
 }  else {
     for ($i = 0; $i < count($user[0]); $i++){ // Om den hittar användarna kör en for loop. Som skriver ut en tabell med de olika värderna.
         	
-		echo '<table border="1">';
+		echo '<table class="admin_table" border="1">';
 		echo '<tr>';
-		echo '<td>'.$user[0][$i]["SSN"].'</td>';
-		echo '<td><form action="/adminuser.php" method="post"><input type="submit" value="Ta bort" name="delete"></form></td>';
+		echo '<td class="admin_td">'.$user[0][$i]["SSN"].'</td>';
+		echo '<td class="admin_td"><form action="/adminuser.php" method="post"><input type="submit" value="Ta bort" name="delete"></form></td>';
 		echo '</tr>';
 		echo '</table>';
 					
